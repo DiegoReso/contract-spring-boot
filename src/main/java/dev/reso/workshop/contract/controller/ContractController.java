a55223cad45ae0a7d1bdda2bd5e61343e27c2e48
diego reis <diegoreso@me.com>
2025-07-10 15:05:47 -0300

    feat: change the return endpoint for creating new contract

--- src/main/java/dev/reso/workshop/contract/controller/ContractController.java
+++ src/main/java/dev/reso/workshop/contract/controller/ContractController.java
@@ -1,15 +1,33 @@
 package dev.reso.workshop.contract.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.reso.workshop.contract.controller.request.ContractRequest;
+import dev.reso.workshop.contract.controller.response.ContractResponse;
+import dev.reso.workshop.contract.entities.Contract;
+import dev.reso.workshop.contract.mapper.ContractMapper;
+import dev.reso.workshop.contract.service.ContractService;
+import jdk.jfr.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/contracts")
 public class ContractController {
 
+    @Autowired
+    private ContractService service;
+
     @GetMapping
-    public String findAllContracts(){
-        return "Oinn";
+    public List<ContractResponse> findAllContracts(){
+        List<Contract> contracts = service.findAllContracts();
+        return contracts.stream().map(ContractMapper::toContractResponse).toList();
+    }
+
+    @PostMapping
+    public ContractResponse insertConctract(@RequestBody ContractRequest contractRequest){
+        Contract contract = ContractMapper.toContract(contractRequest);
+        Contract newContract = service.insertContract(contract);
+        return ContractMapper.toContractResponse(newContract);
     }
 }
