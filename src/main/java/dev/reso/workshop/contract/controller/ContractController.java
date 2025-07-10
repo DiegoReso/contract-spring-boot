c1b2f6d4e3b0e5ff64450e716b95f2e078773df0
diego reis <diegoreso@me.com>
2025-07-10 17:00:10 -0300

    feat: Add JSON processing exception handling to ContractController

--- src/main/java/dev/reso/workshop/contract/controller/ContractController.java
+++ src/main/java/dev/reso/workshop/contract/controller/ContractController.java
@@ -1,14 +1,23 @@
 package dev.reso.workshop.contract.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.reso.workshop.contract.controller.request.ContractRequest;
 import dev.reso.workshop.contract.controller.response.ContractResponse;
 import dev.reso.workshop.contract.entities.Contract;
+import dev.reso.workshop.contract.exceptions.FileEmptyException;
+import dev.reso.workshop.contract.exceptions.FileStorageException;
 import dev.reso.workshop.contract.mapper.ContractMapper;
 import dev.reso.workshop.contract.service.ContractService;
-import jdk.jfr.Category;
+import dev.reso.workshop.contract.util.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,6 +26,11 @@ public class ContractController {
 
     @Autowired
     private ContractService service;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private FileStorageService fileStorageService;
+
 
     @GetMapping
     public List<ContractResponse> findAllContracts(){
@@ -25,9 +39,20 @@ public class ContractController {
     }
 
     @PostMapping
-    public ContractResponse insertConctract(@RequestBody ContractRequest contractRequest){
+    public ResponseEntity<ContractResponse>  insertContract(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("contractData") String contractJson) throws JsonProcessingException {
+
+        if (file.isEmpty()) {
+            throw new FileEmptyException("File is empty, choose a pdf file");
+        }
+
+        String pdfFilePath = fileStorageService.saveFile(file);
+        ContractRequest contractRequest = objectMapper.readValue(contractJson, ContractRequest.class);
         Contract contract = ContractMapper.toContract(contractRequest);
+        contract.setPdfPathFile(pdfFilePath);
         Contract newContract = service.insertContract(contract);
-        return ContractMapper.toContractResponse(newContract);
+        ContractResponse contractResponse = ContractMapper.toContractResponse(newContract);
+        return new ResponseEntity<>(contractResponse, HttpStatus.CREATED);
     }
 }
