c1a20db61b414d59b091cceeadbc7671bb767fa1
diego reis <diegoreso@me.com>
2025-07-10 15:04:30 -0300

    refact: Create new attributes

--- src/main/java/dev/reso/workshop/contract/entities/Contract.java
+++ src/main/java/dev/reso/workshop/contract/entities/Contract.java
@@ -1,18 +1,34 @@
 package dev.reso.workshop.contract.entities;
 
 import dev.reso.workshop.contract.enums.ContractType;
+import dev.reso.workshop.contract.enums.Modality;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(value = "contracts")
 public class Contract {
 
+    @Id
     private String id;
     private String name;
+    private int number;
     private double balance;
     private double total;
     private ContractType type;
+    private String rating;
+    private Manager manager;
+    private String pdfPathFile;
+    private Date initiationContract;
+    private Date endContract;
+    private Modality modality;
 }
