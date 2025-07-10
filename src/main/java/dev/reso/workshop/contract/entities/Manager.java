baaa59a0845af8cf8d311812d4d88b55e78c4603
diego reis <diegoreso@me.com>
2025-07-10 15:03:32 -0300

    feat: Create Manager entity

---
+++ src/main/java/dev/reso/workshop/contract/entities/Manager.java
@@ -0,0 +1,21 @@
+package dev.reso.workshop.contract.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(value = "manager")
+public class Manager {
+    @Id
+    private String id;
+    private String name;
+    private String role;
+    private String division;
+}
