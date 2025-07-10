4ddc53541368ed83f52d81c9eec2c6ddd077aad2
diego reis <diegoreso@me.com>
2025-07-10 15:03:17 -0300

    feat: Add ContractResponse record for contract response

---
+++ src/main/java/dev/reso/workshop/contract/controller/response/ContractResponse.java
@@ -0,0 +1,24 @@
+package dev.reso.workshop.contract.controller.response;
+
+import dev.reso.workshop.contract.entities.Manager;
+import dev.reso.workshop.contract.enums.ContractType;
+import dev.reso.workshop.contract.enums.Modality;
+import lombok.Builder;
+
+import java.util.Date;
+
+@Builder
+public record ContractResponse(String id,
+                               String name,
+                               int number,
+                               double balance,
+                               double total,
+                               ContractType type,
+                               String rating,
+                               Manager manager,
+                               String pdfPathFile,
+                               Date initiationContract,
+                               Date endContract,
+                               Modality modality
+                               ) {
+}
