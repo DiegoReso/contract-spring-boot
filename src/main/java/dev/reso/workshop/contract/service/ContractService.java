92bf52d9d129b5484a19ac60ff3545a5c5eac3ee
diego reis <diegoreso@me.com>
2025-07-10 15:06:25 -0300

    feat: Implement contract creation and retrieval operations

--- src/main/java/dev/reso/workshop/contract/service/ContractService.java
+++ src/main/java/dev/reso/workshop/contract/service/ContractService.java
@@ -1,8 +1,11 @@
 package dev.reso.workshop.contract.service;
 
+import dev.reso.workshop.contract.entities.Contract;
 import dev.reso.workshop.contract.repository.ContractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContractService {
@@ -10,5 +13,11 @@ public class ContractService {
     @Autowired
     private ContractRepository repository;
 
+    public Contract insertContract(Contract contractRequest){
+        return repository.insert(contractRequest);
+    }
 
+    public List<Contract> findAllContracts() {
+        return repository.findAll();
+    }
 }
