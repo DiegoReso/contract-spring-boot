ecf495d3034d931be407eb2516717281dc71ada8
diego reis <diegoreso@me.com>
2025-07-10 16:58:59 -0300

    refact: Enhance FileStorageService with custom exception handling

--- src/main/java/dev/reso/workshop/contract/util/FileStorageService.java
+++ src/main/java/dev/reso/workshop/contract/util/FileStorageService.java
@@ -1,5 +1,6 @@
 package dev.reso.workshop.contract.util;
 
+import dev.reso.workshop.contract.exceptions.FileStorageException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,21 +17,31 @@ public class FileStorageService {
     @Value("${app.upload.dir}")
     private String uploadDir;
 
-    public String saveFile(MultipartFile file) throws IOException {
-        Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
-        Files.createDirectories(uploadPath);
 
-        String originalFilename = file.getOriginalFilename();
-        String fileExtension = "";
-        int dotIndex = originalFilename.lastIndexOf('.');
-        if (dotIndex > 0) {
-            fileExtension = originalFilename.substring(dotIndex);
+        public String saveFile(MultipartFile file) {
+            Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+
+            try {
+                Files.createDirectories(uploadPath);
+            } catch (IOException ex) {
+                throw new FileStorageException("Could not create upload directory: " + uploadPath);
+            }
+
+            String originalFilename = file.getOriginalFilename();
+            String fileExtension = "";
+            int dotIndex = originalFilename.lastIndexOf('.');
+            if (dotIndex > 0) {
+                fileExtension = originalFilename.substring(dotIndex);
+            }
+            String fileName = UUID.randomUUID().toString() + fileExtension;
+
+            Path filePath = uploadPath.resolve(fileName);
+
+            try {
+                Files.copy(file.getInputStream(), filePath);
+                return filePath.toString();
+            } catch (IOException ex) {
+                throw new FileStorageException("Could not store file " + originalFilename + ". Please, try again!");
+            }
         }
-        String fileName = UUID.randomUUID().toString() + fileExtension;
-
-        Path filePath = uploadPath.resolve(fileName);
-        Files.copy(file.getInputStream(), filePath);
-
-        return filePath.toString();
-    }
 }
