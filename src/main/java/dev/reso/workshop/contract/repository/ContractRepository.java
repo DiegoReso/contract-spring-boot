787f1b1a8fb83b9fd5e586d2d373f312c9dea410
diego reis <diegoreso@me.com>
2025-07-17 11:45:53 -0300

    Feat: Add contract search by manager name

--- src/main/java/dev/reso/workshop/contract/repository/ContractRepository.java
+++ src/main/java/dev/reso/workshop/contract/repository/ContractRepository.java
@@ -2,8 +2,15 @@ package dev.reso.workshop.contract.repository;
 
 import dev.reso.workshop.contract.entities.Contract;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ContractRepository extends MongoRepository<Contract, String> {
+
+    @Query("{ 'manager.name' : { $regex :  ?0, $options: 'i'} }")
+    List<Contract>  findContractByManager(String manager);
+
 }
