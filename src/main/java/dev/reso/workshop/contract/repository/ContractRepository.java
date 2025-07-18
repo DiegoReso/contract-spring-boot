f6352965d1d18ec2b6e09e454d48f14ca2d97fac
diego reis <diegoreso@me.com>
2025-07-18 20:30:01 -0300

    feat: Add method to find contracts by type in repository

--- src/main/java/dev/reso/workshop/contract/repository/ContractRepository.java
+++ src/main/java/dev/reso/workshop/contract/repository/ContractRepository.java
@@ -1,6 +1,7 @@
 package dev.reso.workshop.contract.repository;
 
 import dev.reso.workshop.contract.entities.Contract;
+import dev.reso.workshop.contract.enums.ContractType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,5 @@ public interface ContractRepository extends MongoRepository<Contract, String> {
     @Query("{ 'manager.name' : { $regex :  ?0, $options: 'i'} }")
     List<Contract>  findContractByManager(String manager);
 
+    List<Contract> findContractByType(ContractType type);
 }
