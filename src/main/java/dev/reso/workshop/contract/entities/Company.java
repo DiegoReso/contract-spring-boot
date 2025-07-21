97b9ce07d588438f40c4ede2c1ddada31e45d6a2
diego reis <diegoreso@me.com>
2025-07-21 14:33:27 -0300

    feat: Remove ID field from Company entity

--- src/main/java/dev/reso/workshop/contract/entities/Company.java
+++ src/main/java/dev/reso/workshop/contract/entities/Company.java
@@ -4,19 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Document(value = "companies")
 public class Company {
 
-    @Id
-    private String id;
     private String name;
     private String email;
     private String cpnj;
