8142a98aa56e03083fe2c9a60fb3c9414bb9a0a6
diego reis <diegoreso@me.com>
2025-07-20 23:41:06 -0300

    refactor: Remove validation from Company entity; centralize in CompanyRequest DTO

--- src/main/java/dev/reso/workshop/contract/entities/Company.java
+++ src/main/java/dev/reso/workshop/contract/entities/Company.java
@@ -1,9 +1,5 @@
 package dev.reso.workshop.contract.entities;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,25 +17,10 @@ public class Company {
 
     @Id
     private String id;
-    @NotBlank(message = "Name cannot be null or empty")
-    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
-
-    @NotBlank(message = "Email cannot be null or empty")
-    @Email(message = "Email should be valid")
-    @Size(max = 100, message = "Email cannot exceed 100 characters")
     private String email;
-
-    @NotBlank(message = "CPNJ cannot be null or empty")
-    @Pattern(regexp = "\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}", message = "CPNJ must be in the format XX.XXX.XXX/XXXX-XX")
     private String cpnj;
-
-    @NotBlank(message = "Phone cannot be null or empty")
-    @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Phone number is invalid")
     private String phone;
-
-    @NotBlank(message = "Address cannot be null or empty")
-    @Size(max = 255, message = "Address cannot exceed 255 characters")
     private String address;
 
 }
