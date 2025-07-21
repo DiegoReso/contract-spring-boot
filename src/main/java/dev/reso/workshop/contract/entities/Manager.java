e53941a5f34c396937e3cf57fd2132fa3aa85ff5
diego reis <diegoreso@me.com>
2025-07-21 14:36:41 -0300

    refact: Remove id from manager entity

--- src/main/java/dev/reso/workshop/contract/entities/Manager.java
+++ src/main/java/dev/reso/workshop/contract/entities/Manager.java
@@ -4,17 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(value = "managers")
 public class Manager {
-    @Id
-    private String id;
     private String name;
     private String role;
     private String division;
